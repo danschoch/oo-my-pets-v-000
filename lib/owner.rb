d0fd2b1d0e31b8bfcccbb0e0135e7ab1442a3a4f
Daniel Schoch <d.schoch90@gmail.com>
2018-08-26 19:17:59 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -5,7 +5,15 @@ class Owner
     @@all << self
   end
 
-  def all
+  def self.all
     @@all
+  end
+
+  def self.count
+    self.all.count
+  end
+
+  def self.reset_all
+    self.all.clear
   end
 end
